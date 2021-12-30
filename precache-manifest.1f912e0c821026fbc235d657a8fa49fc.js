dbe993213510f5ecaa5fbf41eb71f2fa91a5ce35
Michał Bednarczyk <bbednarczyk.michal@gmail.com>
2021-12-30 13:20:00 +0100

    Updates

---
+++ precache-manifest.1f912e0c821026fbc235d657a8fa49fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f427993f9bdca76d6f0",
+    "url": "/app/static/css/main.37800469.chunk.css"
+  },
+  {
+    "revision": "8f427993f9bdca76d6f0",
+    "url": "/app/static/js/main.48287a4d.chunk.js"
+  },
+  {
+    "revision": "127917d16c2343d0c8fa",
+    "url": "/app/static/js/runtime~main.d465ad9e.js"
+  },
+  {
+    "revision": "4c59afacec54cb918c28",
+    "url": "/app/static/js/2.da5fdfa2.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b3f620dc5eabca571731d21ab2894e0e",
+    "url": "/app/index.html"
+  }
+];
